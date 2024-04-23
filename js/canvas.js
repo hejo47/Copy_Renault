64ae67748accf545b7fde78ee9ea1e3e9211efc1
hejo47 <hejo_47@naver.com>
2024-04-23 18:10:23 +0900

    feat: cube 초기화

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,26 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
+// import * as GLTFLoader from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+
+
+// const gltfLoader = new GLTFLoader();
+// gltfLoader.load(
+//   '/models/ilbuni.glb',
+//   gltf=>{
+//   	const ilbuniMesh = gltf.scene.children[0];
+//   	scene.add(ilbuniMesh);
+//   }
+// );
+
+const canvas = document.getElementById("car__view-canvas");
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+canvas.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
