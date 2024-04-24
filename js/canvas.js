0056a431c979bfd611317741bd066d874f6e20cc
hejo47 <hejo_47@naver.com>
2024-04-24 14:06:46 +0900

    feat: Three.js 비활성화, 3번째 섹션 제작

--- js/canvas.js
+++ js/canvas.js
@@ -1,41 +1,53 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-// import * as GLTFLoader from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
 
-
-// const gltfLoader = new GLTFLoader();
-// gltfLoader.load(
-//   '/models/ilbuni.glb',
-//   gltf=>{
-//   	const ilbuniMesh = gltf.scene.children[0];
-//   	scene.add(ilbuniMesh);
-//   }
-// );
 
 const canvas = document.getElementById("car__view-canvas");
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   alpha: true,
 });
-renderer.setSize( window.innerWidth, window.innerHeight );
-canvas.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+windowResize();
 
-camera.position.z = 5;
+function windowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  canvas.appendChild(renderer.domElement);
+}
+
+window.addEventListener("resize", windowResize);
+
+const loader = new GLTFLoader();
+
+loader.load('../src/model/free_1975_porsche_911_930_turbo/scene.gltf', function (gltf) {
+  const model = gltf.scene;
+  scene.add(model);
+
+  model.rotation.y = -0.65;
+  let ambientLight = new THREE.AmbientLight(0xffffff, 10);  // 색상과 강도
+  scene.add(ambientLight);
+
+  let dirLight = new THREE.DirectionalLight(0xffffff, 5);
+  dirLight.position.set(-1, 3, -2);  // 위치 설정
+  scene.add(dirLight);
+}, undefined, function (error) {
+  console.error(error);
+});
+
+camera.position.set(0, 1, 4);
+camera.rotation.x = -.1;
+camera.rotation.y = 0.2;
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 animate();
